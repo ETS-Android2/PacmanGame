2d2f7abebe2983b3d36c75c875e498336d0bb512
omergean1212 <65233993+omergean1212@users.noreply.github.com>
2022-04-09 12:31:12 +0300

    PacmanGame - First time upload

---
+++ Class22B_And_1_PacmanGame/src/main/java/com/omer/mypackman/Player.java
@@ -0,0 +1,38 @@
+package com.omer.mypackman;
+
+public class Player {
+
+    private int xPos;
+    private int yPos;
+    private int direction;
+
+    //Default constructor
+    public Player() { }
+
+    public Player(int x, int y,int direction) {
+        setX(x);
+        setY(y);
+        setDirection(direction);
+    }
+
+    //Setters and Getters
+    public int getX() {
+        return xPos;
+    }
+
+    public void setX(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getY() {
+        return yPos;
+    }
+
+    public void setY(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+}
